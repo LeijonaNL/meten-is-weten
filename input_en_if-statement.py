78d50a5f29d0ef8afab81fc230ccfb231ad1e660
Lennert <99069053@mydavinci.nl>
2021-09-30 09:47:08 +0200

    input en if-statement

---
+++ input_en_if-statement.py
@@ -0,0 +1,8 @@
+# if statement
+
+getal_1 = input('Noem je eerste getal. ')
+getal_2 = input('Noem je tweede getal. ')
+
+if getal_1 > getal_2:
+    max = getal_1
+    print('a is het grootste getal: ' + str(max))
