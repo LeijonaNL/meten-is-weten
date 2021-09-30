174551c37f54d289a7f43bdbb623c1322cc419cf
Lennert <99069053@mydavinci.nl>
2021-09-30 09:49:27 +0200

    elif-statement

--- input_en_if-statement.py
+++ input_en_if-statement.py
@@ -6,3 +6,7 @@ getal_2 = input('Noem je tweede getal. ')
 if getal_1 > getal_2:
     max = getal_1
     print('a is het grootste getal: ' + str(max))
+
+if getal_1 < getal_2:
+    min = getal_1
+    print('a is het kleinste getal: ' + str(min))
